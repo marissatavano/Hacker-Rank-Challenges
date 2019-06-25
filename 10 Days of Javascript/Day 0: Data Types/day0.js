c27eff75fef5a0a9e55a8e3278c7c11a50de3c90
Marissa Tavano <tavanom1@tcnj.edu>
2019-06-25 15:21:25 -0400

    Update day0.js

--- 10 Days of Javascript/Day 0: Data Types/day0.js
+++ 10 Days of Javascript/Day 0: Data Types/day0.js
@@ -1,27 +1,3 @@
-'use strict';
-
-process.stdin.resume();
-process.stdin.setEncoding('utf-8');
-
-let inputString = '';
-let currentLine = 0;
-
-process.stdin.on('data', inputStdin => {
-    inputString += inputStdin;
-});
-
-process.stdin.on('end', _ => {
-    inputString = inputString.trim().split('\n').map(string => {
-        return string.trim();
-    });
-    
-    main();    
-});
-
-function readLine() {
-    return inputString[currentLine++];
-}
-
 /**
 *   The variables 'firstInteger', 'firstDecimal', and 'firstString' are declared for you -- do not modify them.
 *   Print three lines:
@@ -45,20 +21,14 @@ function performOperation(secondInteger, secondDecimal, secondString) {
     const firstString = 'HackerRank ';
     
     // Write code that uses console.log to print the sum of the 'firstInteger' and 'secondInteger' (converted to a Number        type) on a new line.
+    console.log(firstInteger + Number(secondInteger));
     
     
     // Write code that uses console.log to print the sum of 'firstDecimal' and 'secondDecimal' (converted to a Number            type) on a new line.
+    console.log(firstDecimal + Number(secondDecimal));
     
     
     // Write code that uses console.log to print the concatenation of 'firstString' and 'secondString' on a new line. The        variable 'firstString' must be printed first.
+    console.log(firstString + secondString);
     
-}
-
-
-function main() {
-    const secondInteger = readLine();
-    const secondDecimal = readLine();
-    const secondString = readLine();
-    
-    performOperation(secondInteger, secondDecimal, secondString);
 }
