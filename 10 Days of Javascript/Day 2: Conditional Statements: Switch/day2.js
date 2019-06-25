798239ba8599fecaf5919bf5ff04e6f9df19a149
Marissa Tavano <tavanom1@tcnj.edu>
2019-06-25 15:26:01 -0400

    Create day2.js

---
+++ 10 Days of Javascript/Day 2: Conditional Statements: Switch/day2.js
@@ -0,0 +1,47 @@
+function getLetter(s) {
+    let letter;
+    // Write your code here
+    
+    let firstLetter = s.charAt(0);   // Gets the first letter of string 's'.
+    
+    switch(firstLetter) {
+        case 'a':
+        case 'e':
+        case 'i':
+        case 'o':
+        case 'u':
+            letter = "A";
+            break;
+        case 'b':
+        case 'c':
+        case 'd':
+        case 'f':
+        case 'g':
+            letter = "B";
+            break;
+        case 'h':
+        case 'j':
+        case 'k':
+        case 'l':
+        case 'm':
+            letter = "C";
+            break;
+        case 'n':
+        case 'p':
+        case 'q':
+        case 'r':
+        case 's':
+        case 't':
+        case 'v':
+        case 'w':
+        case 'x':
+        case 'y':
+        case 'z':
+            letter = "D"
+            break;
+            
+            
+    }
+    
+    return letter;
+}
